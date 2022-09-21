7c4bce7f51b343d4c10e916ef0dc57f7327c8b0f
nikolaradovanov <52472211+nikolaradovanov@users.noreply.github.com>
2022-09-22 00:07:30 +0200

    Finished example app (#1)

--- src/main/java/com/javatechie/spring/ws/api/controller/ChatController.java
+++ src/main/java/com/javatechie/spring/ws/api/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.javatechie.spring.ws.api.controller;
 
+import com.javatechie.spring.ws.api.services.ChatMessageService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -11,9 +12,20 @@ import com.javatechie.spring.ws.api.model.ChatMessage;
 @Controller
 public class ChatController {
 
+	private final ChatMessageService chatMessageService;
+
+	public ChatController(ChatMessageService chatMessageService) {
+		this.chatMessageService = chatMessageService;
+	}
+
 	@MessageMapping("/chat.register")
 	@SendTo("/topic/public")
 	public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+
+		chatMessageService.save(chatMessage);
+
+		System.out.println(chatMessageService.findAll().size());
+
 		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
 		return chatMessage;
 	}
@@ -21,6 +33,14 @@ public class ChatController {
 	@MessageMapping("/chat.send")
 	@SendTo("/topic/public")
 	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+
+
+		chatMessageService.save(chatMessage);
+
+		for (ChatMessage i : chatMessageService.findAll()) {
+			System.out.println(i.getSender() + " : " + i.getContent());
+		}
+
 		return chatMessage;
 	}
 
---
+++ src/main/java/com/javatechie/spring/ws/api/services/ChatMessageService.java
@@ -0,0 +1,6 @@
+package com.javatechie.spring.ws.api.services;
+
+import com.javatechie.spring.ws.api.model.ChatMessage;
+
+public interface ChatMessageService extends CrudService<ChatMessage,Long> {
+}
